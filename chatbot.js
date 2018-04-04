9b815da9156e5382fdd6c32d45ad17cc6b75ccae
Arturo Moquillaza <amoquillaza2pucp.pe>
2018-04-04 17:24:24 +0000

    agregar stickers

--- chatbot.js
+++ chatbot.js
@@ -9,18 +9,21 @@ winston.level = 'info'
 //error,warn,info,verbose,debug,silly
 
 const city_l = 'Lima'
-const city_p = 'Pucallpa'
 const city_i = 'Ica'
 
 var city = city_l
-const url_l = 'http://api.openweathermap.org/data/2.5/weather?q=Lima,pe&appid=' + token_w + '&lang=es';
-const url_p = 'http://api.openweathermap.org/data/2.5/weather?q=Pucallpa,pe&appid' + token_w + '&lang=es';
+const url_w = 'http://api.openweathermap.org/data/2.5/weather?'
+const url_l = url_w + 'q=Lima,pe&appid=' + token_w + '&lang=es';
 const trigger_l = 'LIMA';
-const trigger_p = 'PUCALLPA';
 const trigger_i = 'ICA';
 const start = '/START';
 
 const bot = new Bot(token, {polling: true});
+
+const sti_bruce  = 'CAADAgADPQADyIsGAAER4mZIkRHRsgI';
+const sti_putin  = 'CAADBAADMQQAAjJQbQAB-nKAEeKNehgC';
+const sti_cumber = 'CAADBAADVwQAAjJQbQABOfFMYKmwNfkC';
+const sti_willis = 'CAADBAAD3QMAAjJQbQAB1hVkkW6QXtoC';
 
 const prepareData = (body) => {
  if(typeof JSON.parse(body).main === "undefined"){
@@ -60,23 +63,23 @@ bot.on('message', (msg) => {
  if (msg.text.toString().toUpperCase() === trigger_l) {
   return request(url_l, (err, resp, body) => {
    bot.sendMessage(msg.chat.id, prepareData(body));
+   bot.sendSticker(msg.chat.id, sti_putin);
   });
- } else if (msg.text.toString().toUpperCase() === trigger_p) {
-  return request(url_p, (err, resp, body) => {
-   bot.sendMessage(msg.chat.id, prepareData(body));
-  }); 
  } else if (msg.text.toString().toUpperCase() === start) {
-  return request(url_p, (err, resp, body) => {
-   bot.sendMessage(msg.chat.id, 'Hola, escribe la ciudad para saber el tiempo y clima.', {
-	reply_markup: {keyboard: [[city_l], [city_p], [city_i]]
+   const usuario = msg.from.username;
+   return request(url_l, (err, resp, body) => {
+   bot.sendMessage(msg.chat.id, 'Hola ' + usuario + ', escribe la ciudad para saber el tiempo y clima.', {
+//   bot.sendMessage(msg.chat.id, 'Hola , escribe la ciudad para saber el tiempo y clima.', {
+	reply_markup: {keyboard: [[city_l], [city_i]]
 	              }
 	});
+   bot.sendSticker(msg.chat.id, sti_bruce);
    });
     winston.log('info', 'Start ejecutado', {'comando': start});	  
  } 
  else {
    city = msg.text.toString().toLowerCase();
-   const url = 'http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + token_w;
+   const url = url_w + 'q=' + city + '&appid=' + token_w + '&lang=es';
    return request(url, (err, resp, body) => {
    bot.sendMessage(msg.chat.id, 'Buscando datos para esta ciudad: ' + city + '\n' + prepareData(body));
   }); 
